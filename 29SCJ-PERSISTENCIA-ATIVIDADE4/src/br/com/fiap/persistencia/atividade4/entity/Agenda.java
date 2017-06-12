b72f81ea1f4bb09110dc01adec54b769813ca6bc
Fabiano Tessarolo <fabiano.tessarolo@gmail.com>
2017-06-11 22:54:56 -0300

    Added Agenda Entity

--- 29SCJ-PERSISTENCIA-ATIVIDADE4/src/br/com/fiap/persistencia/atividade4/entity/Agenda.java
+++ 29SCJ-PERSISTENCIA-ATIVIDADE4/src/br/com/fiap/persistencia/atividade4/entity/Agenda.java
@@ -1,5 +1,59 @@
 package br.com.fiap.persistencia.atividade4.entity;
 
-public class Agenda {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="agenda")
+public class Agenda implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8673968137247577173L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private Date data;
+	private Date hora;
+	private String descricao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Date getHora() {
+		return hora;
+	}
+
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 }
